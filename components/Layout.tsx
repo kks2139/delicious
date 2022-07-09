04018938ce1d04810b7d6bf7a1663c8793160e01
kwangsun <eyeyel@naver.com>
2022-07-09 18:07:55 +0900

    feat: doc title 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,21 @@
+import Head from 'next/head';
 import Header from './Header';
+
+const appTitle = '쿠킹';
 
 interface LayoutProps {
   children: React.ReactNode;
+  documentTitle?: string;
 }
 
-function Layout({children}: LayoutProps) {
+function Layout({children, documentTitle = ''}: LayoutProps) {
   return (
     <div className="Layout">
+      <Head>
+        <title>
+          {documentTitle ? `${appTitle} | ${documentTitle}` : appTitle}
+        </title>
+      </Head>
       <Header />
       <div className="content-box">{children}</div>
       <style jsx>{`
